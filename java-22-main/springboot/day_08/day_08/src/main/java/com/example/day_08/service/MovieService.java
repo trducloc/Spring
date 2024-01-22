d4f7b59bc8e4c11d44ceb7a79d2a7a7df247f31e
Tran Duc Loc <tranducloc0123456789@gmail.com>
2024-01-22 11:22:18 +0700

    80%

--- java-22-main/springboot/day_08/day_08/src/main/java/com/example/day_08/service/MovieService.java
+++ java-22-main/springboot/day_08/day_08/src/main/java/com/example/day_08/service/MovieService.java
@@ -19,22 +19,18 @@ public class MovieService {
     }
 
     public List<Movie> getNewMoviesByType(MovieType movieType) {
-        // No need to convert MovieType to String and then back to MovieType
         return movieRepository.getNewMoviesByType(movieType);
     }
 
     public Movie findMovieById(Integer id) {
-        // It's good practice to handle null cases explicitly
         return movieRepository.findMovieById(id).orElse(null);
     }
 
     public List<Movie> getHotMovies() {
-        // Method name should reflect the sorting order, so it should be findMoviesOrderByRatingDesc
         return movieRepository.findMoviesOrderByRatingDesc();
     }
 
     public List<Movie> findMovieRelated(MovieType movieType) {
-        // No need to convert MovieType to String and then back to MovieType
         return movieRepository.findMoviesByTypeOrderByRatingDesc(movieType);
     }
 }
